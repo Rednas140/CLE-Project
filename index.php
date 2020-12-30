103eacc370942576eaf6e4411a4112e3f22c82db
rednas140 <sanderjverhoeven@hotmail.com>
2020-12-30 12:37:17 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style/style.css">
+    <meta charset="UTF-8">
+    <title>Laura's Happy Home</title>
+</head>
+
+<body>
+
+<header>
+    <div class="navbar">
+        <h2 class="logo" >Laura's Happy Home</h2>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="reserveren.php">Reserveren</a></li>
+        </ul>
+    </nav>
+    </div>
+</header>
+
+</body>
+</html>
