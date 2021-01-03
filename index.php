0d4afacd858a1ec4bf7a37358810751b4c2a97d5
rednas140 <sanderjverhoeven@hotmail.com>
2021-01-03 23:31:01 +0100

    toevoegen van form en slides

--- index.php
+++ index.php
@@ -24,8 +24,49 @@
     </div>
 </header>
 <div class="content">
+<div class="slideshow-container">
+    <div class="mySlides fade">
+        <div class="numbertext">1 / 2</div>
+        <img src="style/picture1.jpg" style="width:100%">
+    </div>
+
+    <div class="mySlides fade">
+        <div class="numbertext">2 / 2</div>
+        <img src="style/picture2.jpg" style="width:100%">
+    </div>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+    <span class="dot" onclick="currentSlide(1)"></span>
+    <span class="dot" onclick="currentSlide(2)"></span>
+</div>
     <p>in dit gedeelte komt alle content die de pagina heeft.</p>
 </div>
+
+<script>
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+        setTimeout(showSlides, 3000); // Change image every 2 seconds
+    }
+</script>
+
 </body>
 
 </html>
